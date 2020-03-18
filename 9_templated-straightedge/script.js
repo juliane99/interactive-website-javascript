4b1c119a9c4b5f49cb3c4d8652d01eccfa8d1380
Juliane <57350721+juliane99@users.noreply.github.com>
2020-03-18 18:57:31 +0100

    Probieren ein on click Button zu erstellen

--- 9_templated-straightedge/script.js
+++ 9_templated-straightedge/script.js
@@ -1,3 +1,7 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
-  }
+document.getElementById("myBtn").addEventListener("click", myfunc);
+document.getElementById("myBtn1").addEventListener("click", myfunc);
+
+
+function myfunc() {
+	document.getElementById("demo").innerHTML = "Hello World";
+}
