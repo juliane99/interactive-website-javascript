69ba3eee80cccf897e070d2e9dbe877f2ca8701a
Juliane <57350721+juliane99@users.noreply.github.com>
2020-03-23 19:44:18 +0100

    problems with saving files in sript js.

--- 9_templated-straightedge/script.js
+++ 9_templated-straightedge/script.js
@@ -28,24 +28,7 @@ function myfunction1() {
    }
 
 // interaction3
-var picture = 1;  
-slides(picture);  
-function following(n) {  
-	slides(picture += n);  
-}  
-function currentSlide(n) {  
-	slides(picture = n);  
-}  
-function slides(n) {  
-	var i;  
-	var slides = document.getElementsByClassName("view");  
-	if (n > slides.length) { picture = 1 }  
-	if (n < 1) { picture = slides.length }  
-	for (i = 0; i < slides.length; i++) {  
-		slides[i].style.display = "none";  
-	}  
-	slides[picture - 1].style.display = "block";  
-}  
+
   
   // interaction4
   function filloutform() {
