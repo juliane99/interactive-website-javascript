330a824051d04df8d0d9def60b4fea8eaa42edcb
Juliane <57350721+juliane99@users.noreply.github.com>
2020-03-26 14:34:38 +0100

    adding html and css
    adding some html and css on the pages.

--- 9_templated-straightedge/script.js
+++ 9_templated-straightedge/script.js
@@ -1,4 +1,5 @@
 //interaction1 
+
 window.addEventListener("load", getElements);
 var btn;
 function getElements() {
@@ -26,10 +27,9 @@ function myfunction1() {
 	 }
 	 document.getElementById("thisone").innerHTML = text;
    }
-  
- // interaction4
- 
-//interaction 5
+
+
+//interaction 3
 var string = "JOIN OUR TEAM!";
 var interactiveText = string.split("");
 var getit = document.getElementById('interactiveText');
@@ -41,3 +41,24 @@ var running = setTimeout(animate, 80);
 })();
 
 }
+
+// interaction4
+
+var picture = 1;  
+slides(picture);  
+function following(n) {  
+	slides(picture += n);  
+}  
+function currentSlide(n) {  
+	slides(picture = n);  
+}  
+function slides(n) {  
+	var i;  
+	var slides = document.getElementsByClassName("view");  
+	if (n > slides.length) { picture = 1 }  
+	if (n < 1) { picture = slides.length }  
+	for (i = 0; i < slides.length; i++) {  
+		slides[i].style.display = "none";  
+	}  
+	slides[picture - 1].style.display = "block";  
+}  
