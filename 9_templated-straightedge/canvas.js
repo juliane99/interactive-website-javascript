e5037afea6febcc219f8c39f912e027419a9e842
Juliane <57350721+juliane99@users.noreply.github.com>
2020-03-29 10:42:17 +0200

    moving circle with canvas

--- 9_templated-straightedge/canvas.js
+++ 9_templated-straightedge/canvas.js
@@ -2,50 +2,42 @@
 
 var canvas = document.querySelector('canvas');
 
-
-// fit entire width and heigt of browser window 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
+// fit entire width and heigt
+canvas.width = 1255;
+canvas.height = 300;
 
 // new variable 
 
 var context = canvas.getContext('2d'); // draw 2d elements with vriable c
 
-
-// creating a recktangle
-//context.fillStyle = 'rgba(255, 50, 50, 1) ';	
-//context.fillRect(50, 50, 50, 50);
-
 console.log('canvas');
-
-// creating a circle
-//context.beginPath();
-//context.arc(200, 200, 30, 0, Math.PI * 2, false);
-//context.strokeStyle = 'red';
-//context.stroke();
-
-
 
 // anmitaing a circle
 
-var x = 200;
+var x = 50;
 var movex = 4;
-var circleRadius = 50;
+var y = 50;
+var movey = 4;
+var circleRadius = 30;
+
 function animateCircle() {
     requestAnimationFrame(animateCircle); //metha function 
 
-    context.clearRect(0, 0, innerWidth, innerHeight);
+    context.clearRect(0, 0, 1255, 300);
 
     context.beginPath();
-    context.arc(200, 200, 30, 0, Math.PI * 2, false);
-    context.strokeStyle = 'blue';
+    context.arc(x, y, circleRadius, 0, Math.PI * 2, false);
+    context.strokeStyle = 'red';
     context.stroke();
 
-    if (x + circleRadius > innerWidth) {
+    if (x + circleRadius > 1255 || x - circleRadius < 0) { // condition x 
         movex = -movex;
     }
+    if (y + circleRadius > 300 || y - circleRadius < 0) {
+        movey = -movey;
+    } 
     x += movex;
+    y += movey;
     
 }
 animateCircle();
